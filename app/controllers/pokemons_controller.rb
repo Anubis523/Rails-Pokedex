813c47bbec2dd5028a590ca0b2d424255db532ec
= <=>
2018-08-13 19:36:01 -0400

    Finish

---
+++ app/controllers/pokemons_controller.rb
@@ -0,0 +1,49 @@
+class PokemonsController < ApplicationController
+  def index
+    @pokemons = Pokemon.all
+    render :index
+  end
+
+  def new
+    @pokemon = Pokemon.new # unintialized Pokemon
+    render :new
+  end
+
+  def edit
+    @pokemon = Pokemon.find(params[:id]) # initialized Pokemon
+    render :edit
+  end
+
+  def create
+    @pokemon = Pokemon.new(pokemon_params)
+
+    if @pokemon.save
+      redirect_to "/pokemons/#{@pokemon.id}"
+    end
+  end
+
+  def update
+    @pokemon = Pokemon.find(params[:id])
+    @pokemon.update(pokemon_params)
+
+    if @pokemon.save
+      redirect_to "/pokemons/#{@pokemon.id}"
+    end
+  end
+
+  def show
+    @pokemon = Pokemon.find(params[:id])
+    render :show
+  end
+
+  def destroy
+    @pokemon = Pokemon.find(params[:id])
+    @pokemon.destroy
+    redirect_to pokemons_path # '/pokemons'
+  end
+
+  private
+  def pokemon_params
+    params.require(:pokemon).permit(:name, :element_type, :weight)
+  end
+end
